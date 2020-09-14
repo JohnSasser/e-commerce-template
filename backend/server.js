988017a9f41567c0583b22a999c0049930986d7c
John Sasser <cw.john.sasser@gmail.com>
2020-09-13 23:15:47 -0400

    rest of the app

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import data from './data';
+
+const app = express();
+const PORT = 5000;
+
+app.get('/api/products', (req, res) => {
+  res.send(data.products);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started at http://localhost:${PORT}`);
+});
